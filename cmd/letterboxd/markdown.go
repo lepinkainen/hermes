17d2a0261054c2f8ab1467c7a360f8b53b56ef2c
Riku Lindblad <riku.lindblad@iki.fi>
2025-03-10 22:53:47 +0200

    Update letterboxd markdown

--- cmd/letterboxd/markdown.go
+++ cmd/letterboxd/markdown.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 	"text/template"
 
 	"github.com/lepinkainen/hermes/internal/fileutil"
@@ -12,7 +11,8 @@ import (
 )
 
 const movieTemplate = `---
-title: {{.Name}}
+title: "{{.Name}}"
+type: movie
 year: {{.Year}}
 date_watched: {{.Date}}
 letterboxd_uri: {{.LetterboxdURI}}
@@ -20,40 +20,67 @@ letterboxd_id: {{.LetterboxdID}}
 {{- if .ImdbID}}
 imdb_id: {{.ImdbID}}
 {{- end}}
-{{- if .Director}}
-director: {{.Director}}
+{{- if .Rating}}
+letterboxd_rating: {{.Rating}}
 {{- end}}
-{{- if .Cast}}
-cast:
-{{- range .Cast}}
-  - {{.}}
-{{- end}}
+{{- if .Runtime}}
+runtime_mins: {{.Runtime}}
+duration: {{formatDuration .Runtime}}
 {{- end}}
 {{- if .Genres}}
 genres:
 {{- range .Genres}}
-  - {{.}}
+  - "{{.}}"
 {{- end}}
 {{- end}}
-{{- if .Runtime}}
-runtime: {{.Runtime}}
+{{- if .Director}}
+directors:
+  - "{{.Director}}"
 {{- end}}
-{{- if .Rating}}
-rating: {{.Rating}}
-{{- end}}
-{{- if .PosterURL}}
-poster: {{.PosterURL}}
-{{- end}}
+tags:
+  - letterboxd/movie
+  {{- if .Rating}}
+  - rating/{{.Rating}}
+  {{- end}}
+  - year/{{if ge .Year 2020}}2020s{{else if ge .Year 2010}}2010s{{else if ge .Year 2000}}2000s{{else if ge .Year 1990}}1990s{{else if ge .Year 1980}}1980s{{else if ge .Year 1970}}1970s{{else if ge .Year 1960}}1960s{{else if ge .Year 1950}}1950s{{else}}pre-1950s{{end}}
 ---
-{{if .Description}}
-{{.Description}}
-{{end}}
+
+{{- if .PosterURL}}
+![][{{.PosterURL}}]
+{{- end}}
+
+{{- if .Description}}
+
+>[!summary]- Plot
+> {{.Description}}
+{{- end}}
+
+{{- if .Cast}}
+
+>[!cast]- Cast
+> {{- range .Cast}}
+> - {{.}}
+{{- end}}
+{{- end}}
+
+>[!info]- Letterboxd
+> [View on Letterboxd]({{.LetterboxdURI}})
+{{- if .ImdbID}}
+> [View on IMDb](https://www.imdb.com/title/{{.ImdbID}})
+{{- end}}
 `
+
+// Helper function to format runtime into hours and minutes
+func formatDuration(minutes int) string {
+	hours := minutes / 60
+	mins := minutes % 60
+	return fmt.Sprintf("%dh %dm", hours, mins)
+}
 
 // writeMovieToMarkdown writes a single movie to a markdown file
 func writeMovieToMarkdown(movie Movie, directory string) error {
 	// Create a sanitized title
-	sanitizedTitle := sanitizeFilename(movie.Name)
+	sanitizedTitle := fileutil.SanitizeFilename(movie.Name)
 
 	// For year-specific filenames, we need to customize the path
 	filename := fmt.Sprintf("%s (%d)", sanitizedTitle, movie.Year)
@@ -72,8 +99,13 @@ func writeMovieToMarkdown(movie Movie, directory string) error {
 		}
 	}
 
-	// Parse the template
-	tmpl, err := template.New("movie").Parse(movieTemplate)
+	// Parse the template with custom functions
+	funcMap := template.FuncMap{
+		"formatDuration": formatDuration,
+		"ge":             func(a, b int) bool { return a >= b },
+	}
+
+	tmpl, err := template.New("movie").Funcs(funcMap).Parse(movieTemplate)
 	if err != nil {
 		return fmt.Errorf("failed to parse template: %v", err)
 	}
@@ -97,16 +129,4 @@ func writeMovieToMarkdown(movie Movie, directory string) error {
 
 	log.Infof("Wrote %s", filePath)
 	return nil
-}
-
-// sanitizeFilename replaces invalid filename characters with underscores
-func sanitizeFilename(name string) string {
-	invalidChars := []string{"/", "\\", ":", "*", "?", "\"", "<", ">", "|"}
-	result := name
-
-	for _, char := range invalidChars {
-		result = strings.ReplaceAll(result, char, "_")
-	}
-
-	return result
 }
